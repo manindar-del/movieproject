0b63102ec8bc72ac7f5c12e12342e55f7ef37e1f
manindar <manindar.shah@webskitters.com>
2024-06-24 07:50:02 +0530

    final commit

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -74,10 +74,6 @@ export default function Movie() {
 
           </div>
         </section >
-
-
-
-
       )
       }
     </>
--- pages/tv/[id].js
+++ pages/tv/[id].js
@@ -72,7 +72,6 @@ export default function TV() {
           </section>
         </div>
 
-
       )}
     </>
   )
